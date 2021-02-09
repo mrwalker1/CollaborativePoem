02bf2bb0a5d2eab1cede8d183719324762a0bbda
Miles Walker <m.r.walker1@lancaster.ac.uk>
2021-02-09 17:23:40 +0000

    Class

--- Bricks.java
+++ Bricks.java
@@ -1,13 +1,14 @@
 import java.io.*;
-public class Brick{
-    int position= 0;
-    int populate = 0;
-    public static void 
-    Rectangle brick[] = new Rectangle[10];
-    while(populate<11){
-        position += 105;
-        brick[populate] = new Rectangle(position,100,100,20, "Red");
-        game.addRectangle(brick[populate]);
-        populate++;
+public class Bricks{
+    public static void populate(){
+        int position= 0;
+        int i = 0;
+        Rectangle brick[] = new Rectangle[10];
+        while(i<10){
+            position += 105;
+            brick[i] = new Rectangle(position,100,100,20, "Red");
+            //game.addRectangle(brick[i]);
+            i++;
+        }
     }
 }
