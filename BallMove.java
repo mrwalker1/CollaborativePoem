1df66ea6b58c0d46dba65bf2a0feab2105207b67
Miles Walker <m.r.walker1@lancaster.ac.uk>
2021-02-09 17:56:06 +0000

    Class BallMove added

---
+++ BallMove.java
@@ -0,0 +1,35 @@
+import java.io.*;
+public class BallMove{
+    int hightcheck = 0;
+    int x = 500;
+    int widthcheck = 0;
+    int i = 0;
+    public void move(Ball ball){
+        ball.setYPosition(i);
+        ball.setXPosition(x);
+        if (widthcheck == 0){
+            x+=5;
+        }
+        if (widthcheck==1){
+            x-=5;
+        }
+        if (hightcheck == 0){
+            i+=5;
+        }
+        if (hightcheck == 1){
+            i-=5;
+        }
+        if (i>1000){
+            hightcheck = 1;
+        }
+        if (i<=1){
+            hightcheck=0;
+        }
+        if (x>1000){
+            widthcheck = 1;
+        }
+        if (x<=1){
+            widthcheck =0;
+        }
+    }
+}
--- Screen.java
+++ Screen.java
@@ -2,44 +2,17 @@ import java.io.*;
 public class Screen{
     public static void main(String[] args){
         int i = 1;
-        int j = 0;
-        int x = 500;
-        int xj = 0;
         GameArena game = new GameArena(1000,1000);
         Ball ball = new Ball(0,0,15,"BLUE",1);
         Rectangle player = new Rectangle(500,950,200,30, "Blue");
         Player bat = new Player();
+        BallMove ballmove = new BallMove();
         game.addBall(ball);
         game.addRectangle(player);
         //Brick.populate(game);
         while(i>0){
             bat.move(game,player);
-            ball.setYPosition(i);
-            ball.setXPosition(x);
-            if (xj == 0){
-                x+=5;
-            }
-            if (xj==1){
-                x-=5;
-            }
-            if (j == 0){
-                i+=5;
-            }
-            if (j == 1){
-                i-=5;
-            }
-            if (i>1000){
-                j = 1;
-            }
-            if (i<=1){
-                j=0;
-            }
-            if (x>1000){
-                xj = 1;
-            }
-            if (x<=1){
-                xj=0;
-            }
+            ballmove.move(ball);
             game.pause();
         }
     }
